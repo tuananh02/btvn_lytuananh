509be1331b01f45f7fa66efb24ef87f17ae3eb00
tuananh02 <87473899+tuananh02@users.noreply.github.com>
2022-11-19 16:35:16 +0700

    Add files via upload

---
+++ BTVN_lesson7/javascript.js
@@ -0,0 +1,18 @@
+// Q1 Display th sum of 5 + 10. using 2 variables x and y 
+
+
+//  Q2 Create a variable called z, assign x + y to it, and display the result in an alert box -->
+
+//  Q3 Convert the value of text to lower case. -->
+
+// Display "My name is Rola" by extracting information from the person object with "Rola" is a value of an attribute in the person object-->
+
+
+// Q4 Write a program by use variable, operator and convert it to "hh:mm:ss" format.
+// input: 7826
+// output: 02:10:26 -->
+
+
+// Q5 Write a program to check if a number is prime or not
+// input: 3
+// output: True 
