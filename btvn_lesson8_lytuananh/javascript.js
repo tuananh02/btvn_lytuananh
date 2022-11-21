8e3d1605a3ed0811dae5dbbf5538bf2ad85015af
tuananh02 <87473899+tuananh02@users.noreply.github.com>
2022-11-21 15:49:19 +0700

    Add files via upload

---
+++ btvn_lesson8_lytuananh/javascript.js
@@ -0,0 +1,20 @@
+// Q1- print to the screen even numbers from 1 to 10
+
+// Q2 - using if else  and while to Check prime number.
+// input: 5
+// output: True
+
+
+// Q3-sort the following array in ascending order using loop
+// input: [2,1,7,2,6,9,1,99,12,56]
+// output:[1,1,2,2,6,7,9,12,56,99]
+
+
+// Q4 - sort the following array in descending order using loop
+// input: [2,1,7,2,6,9,1,99,12,56]
+// output:[99,56,12,9,7,6,2,2,1,1]
+
+// Q5-  Given a string is the path of a file in the computer. The correct path is a path that has no leading and trailing slashes (/) and only one slash in between folders to separate it. Please remove the extra slashes to get a correct path
+// input:  ////laptrinh//////code//////javascript/////
+// output: laptrinh/code/javascript
+
